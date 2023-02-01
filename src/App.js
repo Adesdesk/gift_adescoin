4975705085eda1063d0b7e3682cb0585418a2194
Adesdesk <aadelakun28@gmail.com>
2023-02-01 16:53:52 +0100

    Footer and more stylings added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 import ParticlesBg from 'particles-bg';
 
 
@@ -7,34 +8,34 @@ function App() {
     <>
       <NavBar />
       <div>
-        <ParticlesBg color="#EC9706" num={100} type="square" bg={true} />
+        <ParticlesBg color="#ffffff" bg={true} />
         <div className="grid place-items-center container mx-auto rounded-xl shadow border p-8 m-10">
-          <p className="text-3xl text-amber-900 font-bold mb-4">
+          <p className="text-3xl text-fuchsia-800 font-bold mb-4">
             gift_adescoin
           </p>
-          <p className="text-justify text-amber-700 mb-8 text-lg">
+          <p className="text-justify text-slate-900 mb-8 text-lg">
             gift_adescoin decentralized application lets you interact with the adescoin smart contract. This contract
             implements a DeFi system via which Adeola gifts his custom token ADCN (adescoin) to deserving members of the public. Beneficiaries 
             of the adescoin cryptocurrency gift can also send designated amounts of the coin with others as they choose, provided 
             their ADCN account balance is not less than 1ADCN.
           </p>
-          <div class="w-full max-w-xs">
-            <form class="shadow-md rounded px-2 pt-2 pb-2 mb-4">
-              <div class="mb-4">
-                <label class="block grid place-items-center text-amber-900 text-xl font-bold mb-4" for="username">
+          <div className="w-full max-w-xs">
+            <form className="shadow-md rounded px-2 pt-2 pb-2 mb-4">
+              <div className="mb-4">
+                <label className="block grid place-items-center text-fuchsia-800 text-xl font-bold mb-4" htmlFor="username">
                   Address to gift
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-amber-900 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="sample: 0x2tUvr...8Zio91" />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-amber-900 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="sample: 0x2tUvr...8Zio91" />
               </div>
-              <div class="mb-6">
-                <label class="block grid place-items-center text-amber-900 text-xl font-bold mb-2" for="password">
+              <div className="mb-6">
+                <label className="block grid place-items-center text-fuchsia-800 text-xl font-bold mb-2" htmlFor="password">
                   Amount
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-amber-900 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="sample: 65" />
-                <p class="grid place-items-center text-amber-900 text-xs italic">Amount must be a whole number.</p>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-amber-900 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="sample: 65" />
+                <p className="grid place-items-center text-red-600 text-xs italic">Amount must be a whole number.</p>
               </div>
-              <div class="block grid place-items-center">
-                <button class="bg-amber-700 hover:bg-amber-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              <div className="block grid place-items-center">
+                <button className="bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                   Gift
                 </button>
               </div>
@@ -42,10 +43,9 @@ function App() {
           </div>
 
         </div>
-      <p class="text-center text-gray-500 text-xs">
-        &copy;2022 adescoin. All rights reserved.
-      </p>
+      
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="">
+      <footer className="w-full bg-fuchsia-800 shadow">
+      <p className="text-center text-sm text-white">
+        ADCN (adescoin) by Adeola David Adelakun
+        <br></br>
+        &copy;2022. All rights reserved.
+      </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
